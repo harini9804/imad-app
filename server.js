b6171b17e3d293d0a662a1cf2086b653c6fd9a7c
harini9804 <harini9804@gmail.com>
2017-08-06 21:02:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
-  res.send("Article two has been requested and will be served here.");
+  rres.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three', function (req, res) {
   res.send("Article three has been requested and will be served here.");
