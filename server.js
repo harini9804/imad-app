a0e192c59567a537ff566f3cbff61cf8e6f38587
harini9804 <harini9804@gmail.com>
2017-08-09 18:25:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+  
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
@@ -10,7 +14,7 @@ app.use(morgan('combined'));
 var articles = {
 
 'article-One':{
-    
+    title: 'Article One | Harini R',
     date: `August 6, 2017`,
     heading:`Article One`,
     
@@ -89,7 +93,5 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+
 });
