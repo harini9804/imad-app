8a3f885a58a11243db799d56ee43b746ce459669
harini9804 <harini9804@gmail.com>
2017-08-10 17:44:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,6 +69,10 @@ app.get('/', function (req, res) {
 });
 
 
+var counter = 0;
+app.get('/counter', function(req,res){counter = counter+1;
+    res.send(counter.toString());
+});
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
