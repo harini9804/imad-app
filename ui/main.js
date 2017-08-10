68d33a8581045999cddae923716674dadaa3f18a
harini9804 <harini9804@gmail.com>
2017-08-10 18:57:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,3 +29,19 @@ button.onclick = function() {
     
 };
 
+var nameInput = document.getElementById('name');
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    
+  //make req to server and send name
+  //capture list of names and render it
+  var names = ['name1','name2','name3'];
+  var list = '';
+  for(var i=0;i<names.length;i++){
+      list+='<li>'+ names[i]+'</li>';
+    
+}
+var ul = document.getElementById('namelist');
+ul.innerHTML=list;
+
+};
