2c63da09237de5bbdc2b79035a654d273bbe5059
harini9804 <harini9804@gmail.com>
2017-08-10 11:33:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,13 @@ element.innerHTML='new value';
 //move the image
 
 var img= document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
+    
+    var interval = setInterval(moveRight, 100);
     img.style.marginLeft = '300px';
 };
