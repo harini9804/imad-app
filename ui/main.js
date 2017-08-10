07b5b0b36dfc7bfa08a180186e3d460e0258c734
harini9804 <harini9804@gmail.com>
2017-08-10 11:24:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 // change the text of main text file
 var element= document.getElementById('main-text');
 element.innerHTML='new value';
+
+//move the image
+
+var img= document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginleft='100px';
+};
