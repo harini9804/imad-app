2b34703d03164a42638a8d4dc6de50667887a605
harini9804 <harini9804@gmail.com>
2017-08-10 18:11:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,14 +10,18 @@ button.onclick = function() {
     
     
     //capture response in var
-    request.onreadystatechange = function() { if (request.readystate === XMLHttpRequest.DONE)
+    request.onreadystatechange = function() { if (request.readystate === XMLHttpRequest.DONE){
     //ACTION
-    if (request.status ===200){
-        var counter = request.responseText;
-        var span = document.getElementById('counter');
-    span.innerHTML = counter.toString();
-    }
-    }
+            if (request.status ===200){
+                var counter = request.responseText;
+                var span = document.getElementById('counter');
+            span.innerHTML = counter.toString();
+            }
+         }
+         //make request
+         request.open('GET','http://harini9804.imad.hasura-app.io/counter',true);
+         request.send(null);
+    };
     //render variable in correct span
     
     
