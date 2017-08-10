bd2f18c1c564d2c3217aea76483c19db604dbc9f
harini9804 <harini9804@gmail.com>
2017-08-10 18:13:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 //counter code
 
 var button = document.getElementById('counter');
-counter=0;
+
 button.onclick = function() {
    
     
@@ -13,17 +13,17 @@ button.onclick = function() {
     request.onreadystatechange = function() { if (request.readystate === XMLHttpRequest.DONE){
     //ACTION
             if (request.status ===200){
-                var counter = request.responseText; counter = counter +1;
+                var counter = request.responseText; 
                 var span = document.getElementById('counter');
             span.innerHTML = counter.toString();
             }
          }
          //make request
-         request.open('GET','http://harini9804.imad.hasura-app.io/counter',true);
-         request.send(null);
+        
     };
     //render variable in correct span
-    
+     request.open('GET','http://harini9804.imad.hasura-app.io/counter',true);
+         request.send(null);
     
     
 };
