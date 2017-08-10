063a164855c13cf9e40527069ecfbb2f7e11d65d
harini9804 <harini9804@gmail.com>
2017-08-10 18:27:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 //counter code
 
 var button = document.getElementById('counter');
-
+var counter = 0;
 button.onclick = function() {
    
     
@@ -15,7 +15,7 @@ button.onclick = function() {
             if (request.status ===200){
                 var counter = request.responseText; 
                 var span = document.getElementById('count');
-            span.innerHTML = count.toString();
+            span.innerHTML = counter.toString();
             }
          }
          //make request
