956e8120cbeec21b7f5efd165ef1a932b1784e36
harini9804 <harini9804@gmail.com>
2017-08-10 11:19:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 // change the text of main text file
-var element= document.getElementById("main-text");
-element.InnerHTML="new value";
+var element= document.getElementById('main-text');
+element.innerHTML='new value';
