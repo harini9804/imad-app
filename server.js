f1339a82946c173b0da9ef4765b176e184e5e618
harini9804 <harini9804@gmail.com>
2017-08-13 18:10:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,28 +16,7 @@ app.use(morgan('combined'));
 
 
 
-var articles = {
-'article-One':{
-    title: `Article One | Harini R`,
-    date: `August 6, 2017`,
-    heading:`Article One`,
-    
-    
-content:`<p>Here goes my content! Check this page soon!Here goes my content! Check this page soon!Here goes my content! Check this page soon!Here goes my content! Check this page soon!n!Here goes my content! Check this page soon!Here goes my content! Check this page soon!Here goes my content! Check this page soon!Here goes my content! Check this page soon!</p>
-        <p>Here goes my content! Check this page soon!Here goes my content! Check this page soon!Here goes my content! Check this page soon!Here goes my content! Check this page soon!n!Here goes my content! Check this page soon!Here goes my content! Check this page soon!Here goes my content! Check this page soon!Here goes my content! Check this page soon!</p>
-        <p>Here goes my content! Check this page soon!Here goes my content! Check this page soon!Here goes my content! Check this page soon!Here goes my content! Check this page soon!n!Here goes my content! Check this page soon!Here goes my content! Check this page soon!Here goes my content! Check this page soon!Here goes my content! Check this page soon!</p>`},   
-'article-Two':{
-    title: `Article Two | Harini R`,
-    date: `August 7, 2017`,
-    heading:`Article Two`,
-    content:`<p>Here goes my content! Check this second article soon! </p>`},
-'article-Three':{
-    title: `Article Three | Harini R`,
-    date: `August 7, 2017`,
-    heading:`Article Three`,
-    content:`<p>Here goes my content! Check this third article soon! </p>`}
-    
-};
+
 
 function createTemplate(data)
 {   var title = data.title;
