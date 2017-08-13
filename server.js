71ee5d7eab5350b79a2485972f8209ef204d91ef
harini9804 <harini9804@gmail.com>
2017-08-13 17:07:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
+
+var config = {
+    user: 'harini9804',
+    database: 'harini9804',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password:process.env.DB_PASSWORD
+}
 var app = express();
 app.use(morgan('combined'));
 
@@ -67,6 +76,23 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+  //select 
+  pool.query('SELECT * from test', function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+          
+      }else
+      res.send(JSON.stringify(result));
+  });
+  //send the response and get the result
+  
+});
+
+
 
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
