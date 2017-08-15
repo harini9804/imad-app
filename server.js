3b7ad8f6cb899d5b5eb444198c936f738f340b52
harini9804 <harini9804@gmail.com>
2017-08-15 23:22:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,16 +54,18 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  
 });
 
-function hash(input){
+function hash(input,salt){
 
-var hashed = crypto.pbkdf25();
+var hashed = crypto.pbkdf25(input,salt,10000,512,'sha512');
+return hashed.toString('hex');
 
 }
 
 app.get('/hash/:input',function(req,res){
-   var hashedString = hash(req.params.input);
+   var hashedString = hash(req.params.input,'this-is-some-random-string');
    res.send(hashedString);
    
 });
