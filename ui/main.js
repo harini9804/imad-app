5290412e97df3f3c8711fb515ed51829ce86e808
harini9804 <harini9804@gmail.com>
2017-08-16 22:17:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,25 +9,31 @@ submit.onclick = function() {
  request.onreadstatechange = function() {
      if(request.readyState  === XMLHttpRequest.DONE) {
          if(request.status === 200) {
-             var names = request.responseText;
-             names = JSON.parse(names);
-             var list = '';
-            for(var i=0;i<names.length;i++){
-            list+='<li>'+ names[i]+'</li>';
+             console.log('user logged in');
+             alert('logged in succeddfully');
+             
+         }else if (request.status===403){
+         alert('username/password is invalid');}
+         else if (request.status===500) {
+             alert('Something went wrong in the server');
          }
-         var ul = document.getElementById('namelist');
-ul.innerHTML=list;
-     }
+         
+     
+     
      }
  };
   //make req to server and send name
   //capture list of names and render it
  
  
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('POST', 'http://harini9804.imad.hasura-app.io/submit-name?='+name,true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password= document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    
+    
+    request.open('POST', 'http://harini9804.imad.hasura-app.io/login',true);
+    request.send(JSON.stringify({username: username, password: password}));
   
     
 
