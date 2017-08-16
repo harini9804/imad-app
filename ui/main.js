2cfc2948f378fb2b3fc63607e478f49ddf68822b
harini9804 <harini9804@gmail.com>
2017-08-16 22:03:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,47 +1,31 @@
-//counter code
+//submit username password to login
 
-var button = document.getElementById('counter');
-var counter = 0;
-button.onclick = function() {
-   
-    
-    //make request to the counter endpoint
-    var request= new XMLHttpRequest();
-    
-    
-    //capture response in var
-    request.onreadystatechange = function() 
-    { if (request.readystate === XMLHttpRequest.DONE){
-    //ACTION
-            if (request.status ===200){
-                var counter = request.responseText; 
-                var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-            }
-         }
-         //make request
-        
-    };
-    //make request
-    request.open('GET','http://harini9804.imad.hasura-app.io/counter',true);
-         request.send(null);
-    
-    
-};
-
-var nameInput = document.getElementById('name');
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-    
+ 
+ //create request object
+ var request = new XMLHttpRequest();
+ //capture the response and store it in a variable
+ request.onreadstatechange = function() {
+     if(request.readyState  === XMLHttpRequest.DONE) {
+         if(request.status === 200) {
+             var names = request.responseText;
+             names = JSON.parse(names);
+             var list = '';
+            for(var i=0;i<names.length;i++){
+            list+='<li>'+ names[i]+'</li>';
+         }
+         var ul = document.getElementById('namelist');
+ul.innerHTML=list;
+     }
+     }
+ };
   //make req to server and send name
   //capture list of names and render it
-  var names = ['name1','name2','name3'];
-  var list = '';
-  for(var i=0;i<names.length;i++){
-      list+='<li>'+ names[i]+'</li>';
+ 
+  
     
-}
-var ul = document.getElementById('namelist');
-ul.innerHTML=list;
+
+
 
 };
